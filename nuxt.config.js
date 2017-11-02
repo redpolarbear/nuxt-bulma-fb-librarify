e82cee2ca20988f9bb74536bcefb54d1e477e4cd
Matthew Xu <mxu@counterpath.com>
2017-11-02 09:43:26 -0700

    initiated

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt-bulma-fb-librarify',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+      { 'http-equiv': 'content-type', content: 'text/html; charset=UTF-8' },
+      { hid: 'description', name: 'description', content: 'Project Librarify (nuxt+bulma+fb)' }
+    ] // ,
+    // link: [
+    //   { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    // ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+    postcss: {
+      plugins: {
+        'postcss-custom-properties': {
+          warnings: false
+        }
+      }
+    }
+  },
+  /*
+  ** Modules
+  */
+  modules: [
+    '@nuxtjs/bulma',
+    '@nuxtjs/font-awesome',
+    {
+      src: '@rafamaciel/firebase',
+      options: {
+        apiKey: 'AIzaSyBp-VoO4yMdfcJjHeG8rCLYmlovp634-yY',
+        authDomain: 'vuejs-firebase-01-55b96.firebaseapp.com',
+        databaseURL: 'https://vuejs-firebase-01-55b96.firebaseio.com',
+        projectId: 'vuejs-firebase-01-55b96',
+        storageBucket: 'vuejs-firebase-01-55b96.appspot.com',
+        messagingSenderId: '687212823219'
+      }
+    }
+  ]
+}
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <i class="fa fa-book" aria-hidden="true"></i> This is the new nuxt project.
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
