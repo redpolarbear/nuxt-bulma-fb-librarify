66070cad37cb65ecfd6fb8535ca437f52d219490
Matthew Xu <mxu@counterpath.com>
2017-11-02 11:07:01 -0700

    Added the nav bar into the default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
+    <app-navigation></app-navigation>
     <nuxt/>
   </div>
 </template>
+
+<script>
+import NavigationComponent from '@/components/Shared/Navigation'
+
+export default {
+  name: 'defaultLayout',
+  components: {
+    'app-navigation': NavigationComponent
+  }
+}
+</script>
 
 <style>
 
