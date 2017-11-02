37de43f9d8d10f4f820d9440cb7f9e156d18e98e
Matthew Xu <mxu@counterpath.com>
2017-11-02 11:07:30 -0700

    default "/library" page

---
+++ pages/library/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section">
+    <div class="container">
+      This is the /library page.
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
