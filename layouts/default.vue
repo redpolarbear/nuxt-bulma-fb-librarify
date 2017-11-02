9a18f7c6e3d325333166e2c5af2805b48829e514
Matthew Xu <mxu@counterpath.com>
2017-11-02 15:07:42 -0700

    Added authStateChange into the created()

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,11 +7,19 @@
 
 <script>
 import NavigationComponent from '@/components/Shared/Navigation'
+import * as types from '@/types'
 
 export default {
   name: 'defaultLayout',
   components: {
     'app-navigation': NavigationComponent
+  },
+  created () {
+    this.$firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$store.dispatch(types.ACTION_AUTO_SIGNIN, user)
+      }
+    })
   }
 }
 </script>
