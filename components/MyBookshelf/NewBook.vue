662c0c40ee45198cce0e12277eb1034621fd341e
Matthew Xu <mxu@counterpath.com>
2017-11-09 15:29:20 -0800

    Added New Book Modal

---
+++ components/MyBookshelf/NewBook.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="modal" v-bind:class="{ 'is-active': showNewBookModal }">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">{{ collectionName }}</p>
+          <button class="delete" aria-label="close" @click="onDismissModal"></button>
+        </header>
+        <section class="modal-card-body">
+        <!-- Content ... -->
+        </section>
+        <footer class="modal-card-foot is-inline-block has-text-right">
+        <button class="button" @click="onDismissModal">Cancel</button>
+        <button class="button is-success">Save</button>
+        </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'newBookModal',
+  props: [ 'collectionName', 'showNewBookModal' ],
+  methods: {
+    onDismissModal () {
+      this.$emit('dismissNewBookModal', false)
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
