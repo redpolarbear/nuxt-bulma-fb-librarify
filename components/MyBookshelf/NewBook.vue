7d45755ad291d3234fbb1e7748ca4e3ead2246e5
Matthew Xu <mxu@counterpath.com>
2017-11-09 19:00:00 -0800

    Added ISBN search input and button

--- components/MyBookshelf/NewBook.vue
+++ components/MyBookshelf/NewBook.vue
@@ -2,17 +2,34 @@
   <div class="modal" v-bind:class="{ 'is-active': showNewBookModal }">
     <div class="modal-background"></div>
     <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">{{ collectionName }}</p>
-          <button class="delete" aria-label="close" @click="onDismissModal"></button>
-        </header>
-        <section class="modal-card-body">
-        <!-- Content ... -->
-        </section>
-        <footer class="modal-card-foot is-inline-block has-text-right">
+      <header class="modal-card-head">
+        <p class="modal-card-title">{{ collectionName }}</p>
+        <button class="delete" aria-label="close" @click="onDismissModal"></button>
+      </header>
+      <section class="modal-card-body">
+        <div class="field has-addons has-addons-centered">
+          <p class="control"><a class="button is-static is-large"><span class="icon is-medium"><i class="fa fa-barcode fa-lg"></i></span></a></p>
+          <p class="control">
+            <input class="input is-large" type="text" placeholder="ISBN10 or ISBN13">
+          </p>
+          <p class="control">
+            <button class="button is-large">
+              <span class="icon is-medium">
+                <i class="fa fa-camera fa-lg"></i>
+              </span>
+            </button>
+          </p>
+        </div>
+        <p class="has-text-centered">
+          <a class="button is-info is-medium">
+            <span>Search</span>
+          </a>
+        </p>
+      </section>
+      <footer class="modal-card-foot is-inline-block has-text-right">
         <button class="button" @click="onDismissModal">Cancel</button>
         <button class="button is-success">Save</button>
-        </footer>
+      </footer>
     </div>
   </div>
 </template>
