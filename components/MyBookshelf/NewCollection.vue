d197354232abf1028b7635ceed3161eddf296383
Matthew Xu <mxu@counterpath.com>
2017-11-09 19:00:44 -0800

    Removed unused variable

--- components/MyBookshelf/NewCollection.vue
+++ components/MyBookshelf/NewCollection.vue
@@ -2,17 +2,17 @@
   <div class="modal" v-bind:class="{ 'is-active': showNewCollectionModal }">
     <div class="modal-background"></div>
     <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">{{ collectionName }}</p>
-          <button class="delete" aria-label="close" @click="onDismissModal"></button>
-        </header>
-        <section class="modal-card-body">
-        <!-- Content ... -->
-        </section>
-        <footer class="modal-card-foot is-inline-block has-text-right">
+      <header class="modal-card-head">
+        <p class="modal-card-title"></p>
+        <button class="delete" aria-label="close" @click="onDismissModal"></button>
+      </header>
+      <section class="modal-card-body">
+      <!-- Content ... -->
+      </section>
+      <footer class="modal-card-foot is-inline-block has-text-right">
         <button class="button" @click="onDismissModal">Cancel</button>
         <button class="button is-success">Save</button>
-        </footer>
+      </footer>
     </div>
   </div>
 </template>
