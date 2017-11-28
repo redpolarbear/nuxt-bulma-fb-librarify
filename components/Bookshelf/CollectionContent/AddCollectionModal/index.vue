2f43dfb39138a50676a804f54b6be962fa12b004
Matthew Xu <mxu@counterpath.com>
2017-11-27 17:12:52 -0800

    Changed - template of the AddCollectionModal

--- components/Bookshelf/CollectionContent/AddCollectionModal/index.vue
+++ components/Bookshelf/CollectionContent/AddCollectionModal/index.vue
@@ -1,11 +1,45 @@
 <template>
-  <div>
-
+  <div class="modal" v-bind:class="{ 'is-active': showAddCollectionModal }">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title has-text-justified is-inline-flex">
+          <span class="icon is-medium"><i class="fa fa-edit fa-lg" aria-hidden="true"></i></span>
+          &nbsp;Edit
+        </p>
+        <button class="delete" aria-label="close" @click="onDismissModal"></button>
+      </header>
+      <section class="modal-card-body">
+        <div class="field">
+          <label class="label">Name</label>
+          <div class="control">
+            <input class="input" type="text">
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Message</label>
+          <div class="control">
+            <textarea class="textarea"></textarea>
+          </div>
+        </div>
+      </section>
+      <footer class="modal-card-foot is-inline-block has-text-right">
+        <button class="button" @click="onDismissModal">Cancel</button>
+        <button class="button is-success">Save</button>
+      </footer>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'addCollectionModal',
+  props: [ 'showAddCollectionModal' ],
+  methods: {
+    onDismissModal () {
+      this.$emit('dismissAddCollectionModal', false)
+    }
+  }
 }
 </script>
 
